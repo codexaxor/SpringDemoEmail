081955d5af7761d044d6bdc64eafeef7d7d18296
codexaxor <loicmeta@gmail.com>
2023-07-14 18:39:28 +0600

    cleanup and refactored for single email test

--- src/main/java/io/getarrays/userservice/utils/EmailUtils.java
+++ src/main/java/io/getarrays/userservice/utils/EmailUtils.java
@@ -8,12 +8,8 @@ package io.getarrays.userservice.utils;
  */
 public class EmailUtils {
 
-    public static String getEmailMessage(String name, String host, String token) {
-        return "Hello " + name + ",\n\nYour new account has been created. Please click the link below to verify your account. \n\n" +
-                getVerificationUrl(host, token) + "\n\nThe support Team";
+    public static String getEmailMessage(String name) {
+        return "Hello " + name + ",\nYour new account has been created";
     }
 
-    public static String getVerificationUrl(String host, String token) {
-        return host + "/api/users?token=" + token;
-    }
 }
